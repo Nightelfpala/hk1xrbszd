030ef6bba605732591a699e4e5aab48330356824
Nightelfpala <padm@freemail.hu>
2016-05-11 23:06:01 +0200

    documentation updates

--- src/testfiles/sections.asm
+++ src/testfiles/sections.asm
@@ -10,7 +10,7 @@ global kezdes
 
 neittkezdj:
 	push dword 126
-	jmp vege
+	jmp near vege
 	
 section .data
 	z: dw 300
@@ -19,7 +19,7 @@ section .text
 
 kezdes:
 	mov eax, 2
-	jmp neittkezdj
+	jmp near neittkezdj
 
 vege:
 	pop ebx
